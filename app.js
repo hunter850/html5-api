001aab7478cb741511c2a2cf532e458b7911382f
hunter850 <hunter850711@gmail.com>
2022-07-06 12:13:25 +0800

    send write

--- app.js
+++ app.js
@@ -18,26 +18,44 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', serveIndex('public', {icons: true}));
+app.use('/', serveIndex('public', { icons: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/try-sse', (req, res) => {
+    let id = 30;
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream; charset=UTF-8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    setInterval(function () {
+        const now = new Date();
+
+        res.write(`id: ${id}\n`);
+        res.write(`data: ${now.toString()}\n\n`);
+
+        id++;
+    }, 1200);
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
